6c6e04d49327792bda57666d1a4ea4f1632f44a3
janel lucia <thejanellucia@gmail.com>
2018-05-10 13:13:17 -0400

    style and layout updates

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,124 +1,65 @@
-'use strict';
+"use strict";
 
-//client testimonial
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 100) {
+        $("nav").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $("nav").removeClass("active");
+    }
+});
 
 $(document).ready(function () {
-    // Scroll Reveal
-    //     window.sr = ScrollReveal();
-    //     sr.reveal('.title-header, .about-wrapper, .consult-wrapper, .team-container, .service-item');
 
-    //   $('.title-header').on('inview', function(event, isInView) {
-    //       if (isInView) {
-    //         $(this).find('img').addClass('jsColorShow');
-    //       } else {
-    //         $(this).find('img').removeClass('jsColorShow');
-    //       }
-    //     });
+    /*============================================================================
+    Hamburger Menu
+    ==============================================================================*/
 
-    //     $('.about-wrapper').on('inview', function(event, isInView) {
-    //       if (isInView) {
-    //         $(this).find('img').addClass('jsColorShow');
-    //       } else {
-    //         $(this).find('img').removeClass('jsColorShow');
-    //       }
-    //     });
-
-    //      $('.consult-wrapper').on('inview', function(event, isInView) {
-    //       if (isInView) {
-    //         $(this).find('img').addClass('jsColorShow');
-    //       } else {
-    //         $(this).find('img').removeClass('jsColorShow');
-    //       }
-    //     });
-
-    //     $('.team-container').on('inview', function(event, isInView) {
-    //       if (isInView) {
-    //         $(this).find('img').addClass('jsColorShow');
-    //       } else {
-    //         $(this).find('img').removeClass('jsColorShow');
-    //       }
-    //     });
-    //      $('.service-wrapper').on('inview', function(event, isInView) {
-    //       if (isInView) {
-    //         $(this).find('img').addClass('jsColorShow');
-    //       } else {
-    //         $(this).find('img').removeClass('jsColorShow');
-    //       }
-    //     });
-    //     //end of scroll reveal
-
-}); //end of doc ready
-
-
-/*============================================================================
-Back to Top
-==============================================================================*/
-
-$(function () {
-    $(window).scroll(function () {
-
-        $('body').prepend('<a href="#" class="back-to-top"></a>');
-        //back to top button
-        if ($(window).scrollTop() / 500) {
-            $('a.back-to-top').fadeIn('slow');
-        } else {
-            $('a.back-to-top').fadeOut('slow');
-        }
+    $(".cross").hide();
+    $(".hammy-menu").hide();
+    $(".hamburger").click(function () {
+        $(".hammy-menu").slideToggle("slow", function () {
+            $(".hamburger").hide();
+            $(".cross").show();
+            $("nav").removeClass("active");
+        });
     });
 
-    $('a.back-to-top').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 700);
-        return false;
+    $(".cross").click(function () {
+        $(".hammy-menu").slideToggle("slow", function () {
+            $(".cross").hide();
+            $(".hamburger").show();
+        });
     });
-});
 
-/*============================================================================
-Hamburger Menu
-==============================================================================*/
+    /*============================================================================
+    Back to Top
+    ==============================================================================*/
+    // if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+    // 	$('#return-to-top').fadeIn(200);    // Fade in the arrow
+    // } else {
+    //     $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    // }
 
-$(".cross").hide();
-$(".hammy-menu").hide();
-$(".hamburger").click(function () {
-    $(".hammy-menu").slideToggle("slow", function () {
-        $(".hamburger").hide();
-        $(".cross").show();
-    });
-});
 
-$(".cross").click(function () {
-    $(".hammy-menu").slideToggle("slow", function () {
-        $(".cross").hide();
-        $(".hamburger").show();
-    });
-});
-
-/*============================================================================
-Front Page Flexslider
-==============================================================================*/
-$(document).ready(function () {
-
+    /*============================================================================
+    Front Page Flexslider
+    ==============================================================================*/
     //rotation speed and timer
-    var speed = 13000;
+    var speed = 9000;
     var run = setInterval(rotate, speed);
 
     var slides = $('.slide');
     var container = $('#slides ul');
     var elm = container.find(':first-child').prop("tagName");
-    var item_width = container.width();
+    var item_width = $('#carousel').outerWidth();
     var previous = 'prev'; //id of previous button
     var next = 'next'; //id of next button
-    slides.width(item_width); //set the slides to the correct pixel width
-    container.parent().width(item_width);
+
+    $('#slides ul li').css('width', item_width);
     container.width(slides.length * item_width); //set the slides container to the correct total width
     container.find(elm + ':first').before(container.find(elm + ':last'));
-    resetSlides();
-
-    // $('#slides ul li').css('width', item_width);
-    // container.width(slides.length * item_width); //set the slides container to the correct total width
-    // container.find(elm + ':first').before(container.find(elm + ':last'));
-    // resetSlides(item_width);
+    resetSlides(item_width);
 
     $(window).resize(function () {
         item_width = $('#carousel').width();
@@ -126,6 +67,7 @@ $(document).ready(function () {
         $('#slides ul li').css('width', item_width);
         resetSlides(item_width);
     });
+
     //if user clicked on prev button
     $('#slide-buttons button').click(function (e) {
         //slide the item
@@ -153,18 +95,34 @@ $(document).ready(function () {
         return false;
     });
 
-    function resetSlides() {
+    //if mouse hover, pause the auto rotation, otherwise rotate it    
+    container.parent().mouseenter(function () {
+        clearInterval(run);
+    }).mouseleave(function () {
+        run = setInterval(rotate, speed);
+    });
+
+    function resetSlides(width) {
         //and adjust the container so current is in the frame
         container.css({
-            'left': -1 * item_width
+            'left': -1 * width
         });
     }
 }); //end of document ready function
 
-//a function to click next link
+
+//======== a function to click next link =========
 function rotate() {
     $('#next').click();
 }
+
+//============= Back to Top Cont. ================
+$('#return-to-top').click(function () {
+    // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
+});
 
 /*============================================================================
 Services Click and Toggle information
@@ -172,74 +130,86 @@ Services Click and Toggle information
 
 $('.service-item-1').click(function () {
     $(".toggle-1").toggle("fast");
-    $(".info-1").css("background", "#01a7e3");
+    $(".info-1").css("background", "rgba(1, 167, 227, 0.7)");
 });
 $('.service-item-2').click(function () {
     $(".toggle-2").toggle("fast");
-    $(".info-2").css("background", "#01a7e3");
+    $(".info-2").css("background", "rgba(1, 167, 227, 0.7)");
 });
 $('.service-item-3').click(function () {
     $(".toggle-3").toggle("fast");
-    $(".info-3").css("background", "#01a7e3");
+    $(".info-3").css("background", "rgba(1, 167, 227, 0.7)");
 });
 $('.service-item-4').click(function () {
     $(".toggle-4").toggle("fast");
-    $(".info-4").css("background", "#01a7e3");
+    $(".info-4").css("background", "rgba(1, 167, 227, 0.7)");
 });
 $('.service-item-5').click(function () {
     $(".toggle-5").toggle("fast");
-    $(".info-5").css("background", "#01a7e3");
+    $(".info-5").css("background", "rgba(1, 167, 227, 0.7)");
 });
 $('.service-item-6').click(function () {
     $(".toggle-6").toggle("fast");
-    $(".info-6").css("background", "#01a7e3");
+    $(".info-6").css("background", "rgba(1, 167, 227, 0.7)");
 });
 
 $('.service-item-1').mouseover(function () {
     $(".toggle-1").show("fast");
-    $(".info-1").css("background", "#01a7e3");
+    $(".info-1").css("background", "rgba(1, 167, 227, 0.7)");
+    $(".info-1").css("height", "100%");
 });
 $('.service-item-1').mouseleave(function () {
     $(".toggle-1").hide("fast");
     $(".info-1").css("background", "rgba(65, 64, 66, 0.9)");
+    $(".info-1").css("height", "40px");
 });
 $('.service-item-2').mouseover(function () {
     $(".toggle-2").show("fast");
-    $(".info-2").css("background", "#01a7e3");
+    $(".info-2").css("background", "rgba(1, 167, 227, 0.7)");
+    $(".info-2").css("height", "100%");
 });
 $('.service-item-2').mouseleave(function () {
     $(".toggle-2").hide("fast");
     $(".info-2").css("background", "rgba(65, 64, 66, 0.9)");
+    $(".info-2").css("height", "40px");
 });
 $('.service-item-3').mouseover(function () {
     $(".toggle-3").show("fast");
-    $(".info-3").css("background", "#01a7e3");
+    $(".info-3").css("background", "rgba(1, 167, 227, 0.7)");
+    $(".info-3").css("height", "100%");
 });
 $('.service-item-3').mouseleave(function () {
     $(".toggle-3").hide("fast");
     $(".info-3").css("background", "rgba(65, 64, 66, 0.9)");
+    $(".info-3").css("height", "40px");
 });
 $('.service-item-4').mouseover(function () {
     $(".toggle-4").show("fast");
-    $(".info-4").css("background", "#01a7e3");
+    $(".info-4").css("background", "rgba(1, 167, 227, 0.7)");
+    $(".info-4").css("height", "100%");
 });
 $('.service-item-4').mouseleave(function () {
     $(".toggle-4").hide("fast");
     $(".info-4").css("background", "rgba(65, 64, 66, 0.9)");
+    $(".info-4").css("height", "40px");
 });
 $('.service-item-5').mouseover(function () {
     $(".toggle-5").show("fast");
-    $(".info-5").css("background", "#01a7e3");
+    $(".info-5").css("background", "rgba(1, 167, 227, 0.7)");
+    $(".info-5").css("height", "100%");
 });
 $('.service-item-5').mouseleave(function () {
     $(".toggle-5").hide("fast");
     $(".info-5").css("background", "rgba(65, 64, 66, 0.9)");
+    $(".info-5").css("height", "40px");
 });
 $('.service-item-6').mouseover(function () {
     $(".toggle-6").show("fast");
-    $(".info-6").css("background", "#01a7e3");
+    $(".info-6").css("background", "rgba(1, 167, 227, 0.7)");
+    $(".info-6").css("height", "100%");
 });
 $('.service-item-6').mouseleave(function () {
     $(".toggle-6").hide("fast");
     $(".info-6").css("background", "rgba(65, 64, 66, 0.9)");
+    $(".info-6").css("height", "40px");
 });
